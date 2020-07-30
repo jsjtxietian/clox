2dbb0253116cbec7938edcd8bdc7608093a484dc
jsjtxietian <jsjtxietian@outlook.com>
2020-07-30 16:16:03 +0800

    vm sketch

--- chunk.h
+++ chunk.h
@@ -11,7 +11,11 @@ typedef enum
 {
 	OP_CONSTANT,
 	OP_RETURN, //return from the current function
-
+	OP_NEGATE,
+	OP_ADD,
+	OP_SUBTRACT,
+	OP_MULTIPLY,
+	OP_DIVIDE,
 } OpCode;
 
 typedef struct
--- debug.c
+++ debug.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "debug.h"
-#include "value.h"   
+#include "value.h"
 
 static int simpleInstruction(const char *name, int offset)
 {
@@ -8,15 +8,15 @@ static int simpleInstruction(const char *name, int offset)
     return offset + 1;
 }
 
-static int constantInstruction(const char* name, Chunk* chunk, int offset) 
-{                  
-    uint8_t constant = chunk->code[offset + 1];    //constant index             
-    printf("%-16s %4d '", name, constant);                      
-    printValue(chunk->constants.values[constant]);              
+static int constantInstruction(const char *name, Chunk *chunk, int offset)
+{
+    uint8_t constant = chunk->code[offset + 1]; //constant index
+    printf("%-16s %4d '", name, constant);
+    printValue(chunk->constants.values[constant]);
     printf("'\n");
     //OP_CONSTANT is two—one for the opcode and one for the operand
-    return offset + 2;                                              
-}                
+    return offset + 2;
+}
 
 void disassembleChunk(Chunk *chunk, const char *name)
 {
@@ -32,21 +32,34 @@ void disassembleChunk(Chunk *chunk, const char *name)
 int disassembleInstruction(Chunk *chunk, int offset)
 {
     printf("%04d ", offset);
-    if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1]) {
-        printf("   | ");                                                   
-    } else {                                                             
-        printf("%4d ", chunk->lines[offset]);                              
-    }           
+    if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1])
+    {
+        printf("   | ");
+    }
+    else
+    {
+        printf("%4d ", chunk->lines[offset]);
+    }
 
     uint8_t instruction = chunk->code[offset];
     switch (instruction)
     {
-        case OP_CONSTANT:                                          
+        case OP_CONSTANT:
             return constantInstruction("OP_CONSTANT", chunk, offset);
+        case OP_NEGATE:
+            return simpleInstruction("OP_NEGATE", offset);
         case OP_RETURN:
             return simpleInstruction("OP_RETURN", offset);
+        case OP_ADD:
+            return simpleInstruction("OP_ADD", offset);
+        case OP_SUBTRACT:
+            return simpleInstruction("OP_SUBTRACT", offset);
+        case OP_MULTIPLY:
+            return simpleInstruction("OP_MULTIPLY", offset);
+        case OP_DIVIDE:
+            return simpleInstruction("OP_DIVIDE", offset);
         default:
             printf("Unknown opcode %d\n", instruction);
             return offset + 1;
-    }
+        }
 }
